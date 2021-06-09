edfb970b237f50ecfb7bf06282a785d3f33d180a
thomas-tank-corp <tharris@hashicorp.com>
2021-06-09 16:17:55 +0100

    Please enter the commit message for your changes. Lines starting

--- firewall.tf
+++ firewall.tf
@@ -1,16 +1,3 @@
-// resource "google_compute_network" "vpc" {
-//   name                    = "${var.prefix}-vpc"
-//   description             = "VPC network"
-//   auto_create_subnetworks = false
-// }
-
-// resource "google_compute_subnetwork" "subnet" {
-//   name          = "${var.prefix}-subnet"
-//   ip_cidr_range = var.subnet_range
-//   region        = var.region
-//   network       = google_compute_network.vpc.self_link
-// }
-
 resource "google_compute_firewall" "main" {
   name    = "${var.prefix}-firewall"
   network = var.vpc
